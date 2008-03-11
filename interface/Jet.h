a1608b76d8a46776e4adabb0b01b785fcad2be7d
cmsbuild <>
2008-03-11 17:06:12 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- PatCandidates-V00-00-03'.
    Sprout from master 2008-03-11 17:05:54 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Adding preliminary version of flag definitions'
    Cherrypick from master 2008-01-26 20:19:45 UTC Giovanni Petrucciani <sha1-8e354cf6922a028ac31e5a5c5a5d72f318c92145@cern.ch> 'Changing PATObject<T> to hold RefToBase<T> instead of Ref<vector<T>>,':
        src/Photon.cc
    Cherrypick from DataFormats-PatCandidates-B1_6_X 2008-03-07 18:52:55 UTC Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch> 'First commit of the move of jet energy scale corrections from Layer-1 to Layer-0:':
        interface/Jet.h
        src/Jet.cc
        src/classes.h
        src/classes_def.xml

--- interface/Jet.h
+++ interface/Jet.h
@@ -1,37 +1,27 @@
 //
-// $Id$
+// $Id: Jet.h,v 1.6.2.1 2008/03/03 16:45:27 lowette Exp $
 //
 
 #ifndef DataFormats_PatCandidates_Jet_h
 #define DataFormats_PatCandidates_Jet_h
 
 /**
-  \class    pat::Jet Jet.h "DataFormats/PatCandidates/interface/Jet.h"
+  \class    Jet Jet.h "DataFormats/PatCandidates/interface/Jet.h"
   \brief    Analysis-level calorimeter jet class
 
    Jet implements the analysis-level calorimeter jet class within the
    'pat' namespace
 
   \author   Steven Lowette
-  \version  $Id$
+  \version  $Id: Jet.h,v 1.6.2.1 2008/03/03 16:45:27 lowette Exp $
 */
 
 #include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/GenJet.h"
 #include "DataFormats/Candidate/interface/Particle.h"
 #include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
 #include "DataFormats/BTauReco/interface/JetTag.h"
 #include "DataFormats/PatCandidates/interface/PATObject.h"
-#include "DataFormats/BTauReco/interface/TrackIPTagInfo.h"
-#include "DataFormats/BTauReco/interface/TrackProbabilityTagInfo.h"
-//#include "DataFormats/BTauReco/interface/TrackProbabilityTagInfoFwd.h"
-#include "DataFormats/BTauReco/interface/TrackCountingTagInfo.h"
-//#include "DataFormats/BTauReco/interface/TrackCountingTagInfoFwd.h"
-#include "DataFormats/BTauReco/interface/SoftLeptonTagInfo.h"
-//#include "DataFormats/BTauReco/interface/SoftLeptonTagInfoFwd.h"
-
-#include "DataFormats/BTauReco/interface/SecondaryVertexTagInfo.h"
 #include "DataFormats/PatCandidates/interface/JetCorrFactors.h"
 
 
@@ -64,14 +54,7 @@ namespace pat {
       Jet             mcFlavCorrJet() const;
       Jet             wCorrJet() const;
       float           bDiscriminator(std::string theLabel) const;
-      const std::vector<reco::TrackIPTagInfoRef> 
-                      bTagIPTagInfoRef()  const;
-      const std::vector<reco::SoftLeptonTagInfoRef>
-                      bTagSoftLeptonERef() const;
-      const std::vector<reco::SoftLeptonTagInfoRef>
-                      bTagSoftLeptonMRef() const;
-      const std::vector<reco::SecondaryVertexTagInfoRef>
-                      bTagSecondaryVertexTagInfoRef() const;
+      reco::JetTagRef bJetTagRef(std::string theLabel) const;
       float           lrPhysicsJetVar(unsigned int i) const;
       float           lrPhysicsJetVal(unsigned int i) const;
       float           lrPhysicsJetLRval() const;
@@ -87,17 +70,13 @@ namespace pat {
       void            setNoCorrFactor(float noCorrF);
       void            setBResolutions(float bResEt_, float bResEta_, float bResPhi_, float bResA_, float bResB_, float bResC_, float bResD_, float bResTheta_);
       void            addBDiscriminatorPair(std::pair<std::string, float> & thePair);
-      void            addBTagIPTagInfoRef(const reco::TrackIPTagInfoRef & tagRef);
-      void            addBTagSoftLeptonERef(const reco::SoftLeptonTagInfoRef & tagRef);
-      void            addBTagSoftLeptonMRef(const reco::SoftLeptonTagInfoRef & tagRef);
-      void            addBTagSecondaryVertexTagInfoRef(const reco::SecondaryVertexTagInfoRef & tagRef);
+      void            addBJetTagRefPair(std::pair<std::string, reco::JetTagRef> & thePair);
       void            setLRPhysicsJetVarVal(const std::vector<std::pair<float, float> > & varValVec);
       void            setLRPhysicsJetLRval(float clr);
       void            setLRPhysicsJetProb(float plr);
       void            setJetCharge(float jetCharge);
-      
-      
-  public:
+
+    public:
 
       reco::TrackRefVector associatedTracks_;
 
@@ -115,16 +94,13 @@ namespace pat {
       std::vector<float> bCovM_;
       // b-tag related members
       std::vector<std::pair<std::string, float> >           pairDiscriVector_;
+      std::vector<std::pair<std::string, reco::JetTagRef> > pairJetTagRefVector_;
       // jet cleaning members (not used yet)
       std::vector<std::pair<float, float> > lrPhysicsJetVarVal_;
       float lrPhysicsJetLRval_;
       float lrPhysicsJetProb_;
       // jet charge members
       float jetCharge_;
-      std::vector<reco::TrackIPTagInfoRef>         bTagIPTagInfoRef_;
-      std::vector<reco::SoftLeptonTagInfoRef>      bTagSoftLeptonERef_;
-      std::vector<reco::SoftLeptonTagInfoRef>      bTagSoftLeptonMRef_;
-      std::vector<reco::SecondaryVertexTagInfoRef> bTagSecondaryVertexTagInfoRef_;
 
   };
 
