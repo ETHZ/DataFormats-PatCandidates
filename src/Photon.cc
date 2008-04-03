9840b03774a1d1bcbaee843879f73c5e188d78a0
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2008-04-03 18:57:18 +0000

    SuperCluster embedded in the pat::Photon Transparant access using Transient Ref's (improved a bit the doxygen doc)

--- src/Photon.cc
+++ src/Photon.cc
@@ -1,5 +1,5 @@
 //
-// $Id: Photon.cc,v 1.3 2008/01/26 20:19:45 gpetrucc Exp $
+// $Id: Photon.cc,v 1.3.2.1 2008/03/17 17:17:49 gpetrucc Exp $
 //
 
 #include "DataFormats/PatCandidates/interface/Photon.h"
@@ -11,6 +11,7 @@ using pat::Photon;
 /// default constructor
 Photon::Photon() :
     PATObject<PhotonType>(PhotonType(0, reco::Particle::LorentzVector(0, 0, 0, 0), reco::Particle::Point(0, 0, 0), 0, 0, 0)),
+    embeddedSuperCluster_(false),
     photonID_(-1.0) 
 {
 }
@@ -19,6 +20,7 @@ Photon::Photon() :
 /// constructor from PhotonType
 Photon::Photon(const PhotonType & aPhoton) :
     PATObject<PhotonType>(aPhoton),
+    embeddedSuperCluster_(false),
     photonID_(-1.0) 
 {
 }
@@ -27,6 +29,7 @@ Photon::Photon(const PhotonType & aPhoton) :
 /// constructor from ref to PhotonType
 Photon::Photon(const edm::RefToBase<PhotonType> & aPhotonRef) :
     PATObject<PhotonType>(aPhotonRef),
+    embeddedSuperCluster_(false),
     photonID_(-1.0) 
 {
 }
@@ -37,9 +40,27 @@ Photon::~Photon() {
 }
 
 
+/// override the superCluster method from CaloJet, to access the internal storage of the supercluster
+/// this returns a transient Ref which *should never be persisted*!
+reco::SuperClusterRef Photon::superCluster() const {
+  if (embeddedSuperCluster_) {
+    return reco::SuperClusterRef(&superCluster_, 0);
+  } else {
+    return PhotonType::superCluster();
+  }
+}
+
+
 /// return the match to the generated photon
 const reco::Particle * Photon::genPhoton() const {
   return (genPhoton_.size() > 0 ? &genPhoton_.front() : 0);
+}
+
+
+/// method to store the photon's supercluster internally
+void Photon::setSuperCluster(const reco::SuperClusterRef & superCluster) {
+  superCluster_.clear();
+  superCluster_.push_back(*superCluster);
 }
 
 
