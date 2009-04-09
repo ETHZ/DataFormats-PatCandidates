ea71d5afb15dea742e6bffea2a662be6286bcc7e
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-04-09 15:10:44 +0000

    backport of newest/finest JECs from Head

--- interface/JetCorrFactors.h
+++ interface/JetCorrFactors.h
@@ -1,5 +1,5 @@
 //
-// $Id: JetCorrFactors.h,v 1.2.2.2 2009/01/09 14:55:23 auterman Exp $
+// $Id: JetCorrFactors.h,v 1.2.2.3 2009/02/17 18:49:04 rwolf Exp $
 //
 
 #ifndef DataFormats_PatCandidates_JetCorrFactors_h
@@ -15,7 +15,7 @@
    PAT Layer-1.
 
   \author   Giovanni Petrucciani
-  \version  $Id: JetCorrFactors.h,v 1.2.2.2 2009/01/09 14:55:23 auterman Exp $
+  \version  $Id: JetCorrFactors.h,v 1.2.2.3 2009/02/17 18:49:04 rwolf Exp $
 */
 
 #include <vector>
@@ -52,15 +52,11 @@ namespace pat {
    	  /// default Constructor
           JetCorrFactors();
 	  /// constructor by value
-          JetCorrFactors(std::string &label, float l1, float l2, float l3, float l4, FlavourCorrections l5, float l6, FlavourCorrections l7);
+          JetCorrFactors(std::string &label, float l1, float l2, float l3, float l4, FlavourCorrections l5, FlavourCorrections l6, FlavourCorrections l7);
 
           /// default scale factor: raw & L1 & L2 & L3 (ignore -1's)
-          float scaleDefault() const { return fabs(correction(L3)); };
-          /// returns true if a specific correction is available, false otherwise
-          bool  hasCorrection(CorrStep step) const;
-          /// returns true if a specific correction is available, false otherwise
-          bool  hasCorrection(const size_t step, const size_t flavour) const {return hasCorrection((CorrStep)(step<<4|flavour));};
-          /// returns the correction for a jet up to a given step, starting from another step.
+          float scaleDefault() const { return correction(L3); };
+	  /// returns the correction for a jet up to a given step, starting from another step.
           float correction(CorrStep step, CorrStep begin=Raw) const ;
           /// convert a string into a CorrStep
           static CorrStep const corrStep(const std::string &step, const std::string &flavour="");  
@@ -97,7 +93,7 @@ namespace pat {
           /// convert a CorrStep into a flavour code 0-5
           static inline size_t iflav(const CorrStep &cstep ) { return (static_cast<size_t>(cstep)) & 0xF; }
           /// get a flavour correction out of a CorrStep
-          static float getFlavCorr(const FlavourCorrections &, const size_t & flavcode ) ;
+          static float getFlavorCorrection(const FlavourCorrections &, const size_t & flavcode ) ;
   };
 }
 
