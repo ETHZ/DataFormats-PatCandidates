15af87455aaa90b97309ecff1d43fc41820d5c20
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-10-28 13:51:11 +0000

    added pat::JetCorrFactors::CorrectionFactor to the dictionaries

--- src/classes.h
+++ src/classes.h
@@ -179,6 +179,10 @@ namespace {
   std::vector<CaloTower> v_ct;
   edm::Wrapper<std::vector<CaloTower> > wv_ct;
 
+  std::pair<std::string,std::vector<float> > jcfcf;
+  edm::Wrapper<std::pair<std::string,std::vector<float> > > w_jcfcf;
+  std::vector<pat::JetCorrFactors::CorrectionFactor> v_jcfcf;
+  edm::Wrapper<std::vector<pat::JetCorrFactors::CorrectionFactor> > w_v_jcfcf;
   std::vector<pat::JetCorrFactors> v_jcf;
   edm::Wrapper<std::vector<pat::JetCorrFactors> >  w_v_jcf;
   edm::ValueMap<pat::JetCorrFactors> vm_jcf;
